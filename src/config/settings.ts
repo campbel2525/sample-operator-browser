f4aca0f520e8cc0eb263f568b921079d7153f89e
古谷竜一 <campbel@campbel.local>
2025-06-15 01:07:51 +0900

    first commit

---
+++ src/config/settings.ts
@@ -0,0 +1,2 @@
+export const SCREENSHOT_DIR = "data/screenshots"
+export const PROMPT_LOG_DIR = "data/prompts"
---
+++ src/services/chatService.ts
@@ -0,0 +1,98 @@
+// src/services/chatService.ts
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { ChatOpenAI } from "@langchain/openai";
+import {
+  AIMessage,
+} from "@langchain/core/messages";
+// import { DEFAULT_OPEN_AI_MODEL } from "@config/settings";
+
+
+function createChatModel(
+  modelName: string,
+  apiKey: string = process.env.OPENAI_API_KEY!
+): ChatOpenAI {
+  return new ChatOpenAI({
+    model: modelName,
+    apiKey,
+    // temperature: 0.7,
+    // maxTokens: 1000,
+  });
+}
+
+/**
+ * GPT へメッセージを投げる
+ *
+ * @param messages any[] の配列
+ *   - text のみの場合: content="こんにちは"
+ *   - 画像を含む場合: content=[{ type:"text", text:"..." }, { type:"image_url", image_url:{ url:"..." } }]
+ * @param modelName 省略可。DEFAULT_OPEN_AI_MODEL が使われる
+ * @returns GPT の応答テキスト
+ *
+ * テキストのみの場合
+  * ```
+  * [
+  *     {
+  *         "role": "human",
+  *         "content": "こんにちは"
+  *     }
+  * ]
+  * ```
+  *
+  * テキストと画像の場合
+  * ```
+  * [
+  *     {
+  *         "role":"human",
+  *         "content":[
+  *             {
+  *                 "type": "text",
+  *                 "text": "画像に書かれている文字は何ですか？書いている文字だけ教えてください"
+  *             },
+  *             {
+  *                 "type": "image_url",
+  *                 "image_url": {
+  *                     #画像のURL、URLベースのbase64
+  *                     "url": "IMG_URL"
+  *                 }
+  *             }
+  *         ]
+  *     }
+  * ]
+  * ```
+ */
+export async function chat(
+  messages: any[],
+  modelName: string = process.env.OPENAI_AI_MODEL!
+): Promise<string> {
+
+
+
+  const chatModel = createChatModel(modelName);
+  const aiResponse: AIMessage = await chatModel.invoke(messages as any[]);
+  return aiResponse.text;
+}
+
+
+export async function chat_response_json(
+  messages: any[],
+  modelName: string = process.env.OPENAI_AI_MODEL!
+): Promise<string> {
+
+  const answer = await chat(messages, modelName)
+
+  try {
+    const instruction = JSON.parse(
+      answer
+        .trim()
+        .replace(/^```(?:json)?\s*/, '')
+        .replace(/\s*```$/, '')
+    );
+
+    return instruction;
+
+  } catch (e) {
+    throw new Error("AIの返答をJSONとして解釈できませんでした " + answer);
+  }
+}
